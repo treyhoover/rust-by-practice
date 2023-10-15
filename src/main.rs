801db20ee36516ddf76d8008f7856ff1aba2487d
Trey Hoover <treyhoover@gmail.com>
2023-10-15 14:04:08 -0600

    Variables 2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 
-// Fix the error below with least amount of modification to the code
+// Fill the blanks in the code to make it compile
 fn main() {
-    let x: i32 = 5; // Uninitialized but used, ERROR !
-    let y: i32; // Uninitialized but also unused, only a Warning !
-
-    assert_eq!(x, 5);
+    let mut x = 1;
+    x += 2; 
+    
+    assert_eq!(x, 3);
     println!("Success!");
 }
